ade1d78465bbf5a0383afce3c42eac9355c63e6a
wojwyp <wypior.wojciech@gmail.com>
2022-01-30 14:26:42 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jaką operację chcesz wykonąć? Możliwe operaje to: '+', '-', '/', '*'.");
+            var action = Console.ReadLine();
+
+            Console.WriteLine("Podaj pierwszą liczbę:");
+            var number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj drugą liczbę:");
+            var number2 = int.Parse(Console.ReadLine());
+
+            var result = 0;
+
+            switch (action)
+            {
+                case "+":
+                    result = number1 + number2;
+                    break;
+                case "-":
+                    result = number1 - number2;
+                    break;
+                case "/":
+                    result = number1 / number2;
+                    break;
+                case "*":
+                    result = number1 * number2;
+                    break;
+                default:
+                    throw new Exception("Wybrałeś złą operację!");
+            }
+
+            Console.WriteLine("Wynik Twojego działania to: " + result);
+        }
+       
+    }
+}
