2c558bb0588042c57e9ec03cb555138b9e2b09ac
wojwyp <wypior.wojciech@gmail.com>
2022-01-30 22:26:21 +0100

    Refactoring the code

--- Program.cs
+++ Program.cs
@@ -10,36 +10,53 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Jaką operację chcesz wykonąć? Możliwe operaje to: '+', '-', '/', '*'.");
-            var action = Console.ReadLine();
+            try
+            {
+                Console.WriteLine("Podaj pierwszą liczbę:");
 
-            Console.WriteLine("Podaj pierwszą liczbę:");
-            var number1 = int.Parse(Console.ReadLine());
+                var number1 = GetInput();
 
-            Console.WriteLine("Podaj drugą liczbę:");
-            var number2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Podaj drugą liczbę:");
+                var number2 = GetInput();
 
-            var result = 0;
+                Console.WriteLine("Jaką operację chcesz wykonąć? Możliwe operaje to: '+', '-', '/', '*'.");
+                var action = Console.ReadLine();
 
+                var result = Calculate(number1, number2, action);
+
+
+                Console.WriteLine("Wynik Twojego działania to: " + result);
+            }
+            catch(Exception ex)
+            {
+                //logowanie do pliku
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static int GetInput()
+        {
+            if (!int.TryParse(Console.ReadLine(), out int input))
+                throw new Exception("Podana wartość jest nieprawidłowa.");
+
+            return input;
+        }
+
+        private static int Calculate(int number1, int number2, string action)
+        {
             switch (action)
             {
                 case "+":
-                    result = number1 + number2;
-                    break;
+                    return number1 + number2;
                 case "-":
-                    result = number1 - number2;
-                    break;
+                    return number1 - number2;
                 case "/":
-                    result = number1 / number2;
-                    break;
+                    return number1 / number2;
                 case "*":
-                    result = number1 * number2;
-                    break;
+                    return number1 * number2;
                 default:
                     throw new Exception("Wybrałeś złą operację!");
             }
-
-            Console.WriteLine("Wynik Twojego działania to: " + result);
         }
        
     }
